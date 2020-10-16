06a60f75fc00d9c95601e4d52a02e8deef813cf4
oscarwang20 <69322572+oscarwang20@users.noreply.github.com>
2020-10-16 10:04:02 -0400

    Update Checkerboard.java

--- Checkerboard.java
+++ Checkerboard.java
@@ -1,17 +1,18 @@
 public class Checkerboard {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int i = 1;
         String checker = "";
-        while (i <= n) {
-            if (n % 2 != 0)
+        for (int j = 1; j <= n; j++) {
+            if (j % 2 == 0)
                 checker = " ";
             else
                 checker = "";
-            while (i <= n) {
-                
+            for (int i = 0; i <= n; i++) {
+                if (i == n)
+                    System.out.println(checker);
+                else
+                    checker += "* ";
             }
-
         }
     }
 }
