2569410b0ba52dca7b49858843d7922dde81f7ba
oscarwang20 <69322572+oscarwang20@users.noreply.github.com>
2020-10-16 09:36:44 -0400

    Add files via upload

---
+++ Checkerboard.java
@@ -0,0 +1,17 @@
+public class Checkerboard {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int i = 1;
+        String checker = "";
+        while (i <= n) {
+            if (n % 2 != 0)
+                checker = " ";
+            else
+                checker = "";
+            while (i <= n) {
+                
+            }
+
+        }
+    }
+}
